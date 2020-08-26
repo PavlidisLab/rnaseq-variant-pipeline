5427fa63fa9aedbfdd1fca049c44bf0b28fa3c4e
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2020-08-26 10:27:23 -0700

    Use Bioluigi from PyPI

--- setup.py
+++ setup.py
@@ -2,6 +2,4 @@ from setuptools import setup
 
 setup(name='rnaseq-variant-pipeline',
       packages=['rnaseq_variant_pipeline'],
-      install_requires=['luigi==2.8.13', 'bioluigi'],
-      dependency_links=[
-          'git+https://github.com/PavlidisLab/bioluigi.git#egg=bioluigi-0.0.0'])
+      install_requires=['luigi==2.8.13', 'bioluigi'])
