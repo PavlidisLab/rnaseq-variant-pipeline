bd5dfc4367b877f9302039eadaf096a2b4387c0c
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2022-02-23 14:25:43 -0800

    Remove Luigi 2.8.13 requirement

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 
 setup(name='rnaseq-variant-pipeline',
       packages=['rnaseq_variant_pipeline'],
-      install_requires=['luigi==2.8.13', 'bioluigi'])
+      install_requires=['luigi', 'bioluigi'])
